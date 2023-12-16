4d77077d80df34676b58bf2b121c7bdcb583a98d
mark-k-mokaya <mark-k-mokaya.gmail.com>
2023-12-16 19:19:49 +0300

    More image optimizations

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,7 +1,13 @@
 export const Feature = ({icon, label, description}) => {
 	return (
 		<div className="flex flex-col w-[200px] mb-5 flex-initial text-center items-center justify-start whitespace-pre-line">
-			<img src={icon} className="w-24 h-24" alt={label} />
+			<img
+				src={icon}
+				width={24}
+				height={24}
+				className="w-24 h-24"
+				alt={label}
+			/>
 			<p className="font-medium text-dark-90">{label}</p>
 			<p className="text-xs text-dark-90">{description}</p>
 		</div>
