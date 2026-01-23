9d0ff8f6006df3c8d7a8b4d735ed9600f5e70763
mark-k-mokaya <mark-k-mokaya.gmail.com>
2026-01-23 14:39:27 +0300

    Change netlify function to common js

--- netlify/functions/fetch-room-details.js
+++ netlify/functions/fetch-room-details.js
@@ -1,10 +1,5 @@
-import dotenv from 'dotenv';
-import {GoogleSpreadsheet} from 'google-spreadsheet';
-import {JWT} from 'google-auth-library';
-
-// require('dotenv').config();
-// const GoogleSpreadsheet = require('google-spreadsheet').GoogleSpreadsheet;
-// const JWT = require('google-auth-library').JWT;
+const GoogleSpreadsheet = require('google-spreadsheet').GoogleSpreadsheet;
+const JWT = require('google-auth-library').JWT;
 
 const serviceAccountAuth = new JWT({
 	email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
@@ -12,7 +7,7 @@ const serviceAccountAuth = new JWT({
 	scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
-export const handler = async () => {
+module.exports.handler = async () => {
 	try {
 		const doc = new GoogleSpreadsheet(
 			process.env.GOOGLE_SPREADSHEET_ID,
